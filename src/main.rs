83db05f4d91ba72ae5b59241e85acc334127a52b
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-09-14 13:06:19 +0000

    feat: allowing multiple guesses with looping

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,24 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess:");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    // Convert the String the program reads as input into a real number type
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("To small!"),
-        Ordering::Greater => println!("To big!"),
-        Ordering::Equal => println!("Your win!"),
+    loop {
+        println!("Please input your guess:");
+    
+        let mut guess = String::new();
+    
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        // Convert the String the program reads as input into a real number type
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("To small!"),
+            Ordering::Greater => println!("To big!"),
+            Ordering::Equal => println!("Your win!"),
+        }
     }
 }
